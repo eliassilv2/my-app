37099ca98a0fc00aab30f9b7fdebfab3a59e7577
Elias <eliaskiso4@gmail.com>
2023-08-16 21:47:58 -0300

    adicionando imagens ao projeto

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text style={{color:'white'}}>Faz uuuu Eliiii!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: 'red', margin: 2 }}>
+        Faz uuuu Eliiii!
+      </Text>
+      <Image
+        source={require('./assets/fla2.jpg/',)}
+        style={{
+          width: 100,
+          height: 100,
+          padding: 130,
+          margin: 5
+        }}
+      />
+      <Image
+        source={require('./assets/flagremio.jpg/',)}
+        style={{
+          width: 100,
+          height: 100,
+          padding: 130
+        }}
+      />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
